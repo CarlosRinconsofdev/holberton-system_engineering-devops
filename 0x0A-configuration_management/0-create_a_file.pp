0438d43512d8a86448ef83e332b5b81fa5be141b
CarlosRinconsofdev <3901@holbertonschool.com>
2022-05-03 20:56:07 -0500

    t0-README

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# This Puppet manifest is about how to create a file
+
+file { '/tmp/school':
+  ensure  => 'present',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
+
